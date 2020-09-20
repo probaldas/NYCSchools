b248ff933eb35317581878d873e92d1b0e7ce71d
Probal <probal.kumar.das@gmail.com>
2020-09-20 05:08:47 -0400

    Created NYCApplication and performed initialization using dagger.

--- app/src/main/java/com/probaldas/nycSchools/di/AppComponent.java
+++ app/src/main/java/com/probaldas/nycSchools/di/AppComponent.java
@@ -1,5 +1,7 @@
 package com.probaldas.nycSchools.di;
 
+import com.probaldas.nycSchools.NYCApplication;
+
 import javax.inject.Singleton;
 
 import dagger.Component;
@@ -7,4 +9,7 @@ import dagger.Component;
 @Singleton
 @Component(modules = {AppModule.class})
 public interface AppComponent {
+
+    void inject(NYCApplication application);
+
 }
